30ecb59658a351e9c95c7dfa626e392728ec45af
as <as@asdeiMac.local>
2025-01-16 14:47:51 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+ fn calculate_price_of_apples(count: i32) -> i32 {
+    let result = if count <= 40 {
+        count * 2
+    } else {
+        count
+    };
+    result
+ }
 
 // Don't modify this function!
 #[test]
